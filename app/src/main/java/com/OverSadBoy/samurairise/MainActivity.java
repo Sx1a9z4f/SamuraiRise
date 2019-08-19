ca59aba436d674dd103af99e520919c1ba8189ec
SadBoy <paramime151@gmail.com>
2019-08-19 13:16:33 +0400

    create item for list

--- app/src/main/java/com/OverSadBoy/samurairise/MainActivity.java
+++ app/src/main/java/com/OverSadBoy/samurairise/MainActivity.java
@@ -1,14 +1,20 @@
 package com.OverSadBoy.samurairise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recyclerAlarm);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
