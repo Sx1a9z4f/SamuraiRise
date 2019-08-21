28d53b88aebf64d9e891f5d6caae997abbdf34d2
SadBoy <paramime151@gmail.com>
2019-08-21 11:35:20 +0400

    create ActivityAdd

---
+++ app/src/main/java/com/OverSadBoy/samurairise/ActivityAdd.java
@@ -0,0 +1,13 @@
+package com.OverSadBoy.samurairise;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityAdd extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
--- app/src/main/java/com/OverSadBoy/samurairise/MainActivity.java
+++ app/src/main/java/com/OverSadBoy/samurairise/MainActivity.java
@@ -1,27 +1,31 @@
 package com.OverSadBoy.samurairise;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerAlarm);
+        recyclerView = findViewById(R.id.recyclerAlarm);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new ItemAdapter());
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ActivityAdd.class);
+            startActivity(intent);
+        });
     }
-
 }
